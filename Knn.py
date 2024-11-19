9aff717a5a92e6ba045690911dd598f8ecde8524
jobNattwut <155036631+jobNattwut@users.noreply.github.com>
2024-11-19 16:19:31 +0700

    Update Knn.py

--- Knn.py
+++ Knn.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 st.header('Nattawut Limchai')
-st.header('Information Technology')
+st.subheader('Information Technology')
 st.image("./img/myphoto.jpg")
 col1, col2, col3 = st.columns(3)
 
