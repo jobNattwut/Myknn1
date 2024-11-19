8cbae2f71a7ea9baf0d811ffd6d635b13b402252
jobNattwut <155036631+jobNattwut@users.noreply.github.com>
2024-11-19 14:49:21 +0700

    .

--- home.py
+++ home.py
@@ -4,4 +4,9 @@ import pandas as pd
 st.title("💘💘Websit Developing using python💘💘")
 st.header("💣💣Websit Developing using python💣💣")
 
+st.image('./img/myphoto.jpg')
 st.subheader("Nattwut Limchai")
+
+dt=pd.read_csv('./data/iris-3.csv')
+st.header()
+st.write(dt.head(10))
